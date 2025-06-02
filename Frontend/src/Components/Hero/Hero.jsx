330447452bc1e71180a17c1aab4e19a24ef95ac0
asmid <ansarfeysel@gmail.com>
2025-06-02 22:21:21 +0300

    Add announcement functionality

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { CButton } from '@coreui/react';
-import '@coreui/coreui/dist/css/coreui.min.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { CButton } from "@coreui/react";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center flex-grow-1 text-center">
-      <h1 className="display-4 mb-4">Streamline Property Management at Hawassa University</h1>
-      <p className="lead mb-4 text-dark">Track assets, manage requests, and ensure accountability with ease.</p>
-      <Link to="/login"><CButton color="primary" size="lg" >
-        Login to Get Started
-      </CButton></Link>
+      <h1 className="display-4 mb-4">
+        Streamline Property Management at Hawassa University
+      </h1>
+      <p className="lead mb-4 ">
+        Track assets, manage requests, and ensure accountability with ease.
+      </p>
+      <Link to="/login">
+        <CButton style={{ backgroundColor: "#d9534f", color: "#fff" }} size="lg">
+          Login to Get Started
+        </CButton>
+      </Link>
     </div>
   );
 };
--- Frontend/src/Pages/Landing/Landing.jsx
+++ Frontend/src/Pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import Hero from "../../Components/Hero/Hero";
 import Announcements from "../../Components/Announcements/Announcements"; // Import Announcements
-import img from "../../assets/img/hero.png";
+import img from "../../assets/img/hero-two.jpg";
 
 const LandingPage = () => {
   return (
@@ -15,6 +15,7 @@ const LandingPage = () => {
         backgroundRepeat: "no-repeat",
         display: "flex",
         flexDirection: "column",
+        justifyContent: "center",
         color: "white",
         alignItems: "center",
         paddingTop: "50px",
